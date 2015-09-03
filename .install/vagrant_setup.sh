270b2ad4ec6a216d6566245594610c9ca38511d8
Ilya Grishkov <me@ilyagrishkov.com>
2015-09-03 18:35:10 +0200

    Fixed vagrant provision script

--- .install/vagrant_setup.sh
+++ .install/vagrant_setup.sh
@@ -3,8 +3,9 @@ PROJECT_NAME=$1
 PROJECT_HOST_NAME=$2
 VM_IP=$3
 VIRTUALENV_DIR=/sites
-PROJECT_SOURCE=${VIRTUALENV_DIR}/${PROJECT_NAME}/source
 USER_HOME=/home/vagrant
+PROJECT_SOURCE=${VIRTUALENV_DIR}/${PROJECT_NAME}/source
+SETTINGS_FOLDER=${PROJECT_SOURCE}/${PROJECT_NAME}/${PROJECT_NAME}/settings
 
 echo "Environment installation is beginning. This may take a few minutes.."
 
@@ -24,40 +25,41 @@ apt-get -y install python-dev python3-dev libpq-dev
 echo "Installing virtualenvwrapper from pip.."
 pip install virtualenvwrapper fabric
 
-#git clone https://github.com/cubicuboctahedron/dotfiles.git && sudo su - vagrant /bin/bash -c "set -- -f; source ~/dotfiles/bootstrap.sh"
+git clone https://github.com/cubicuboctahedron/dotfiles.git && sudo su - vagrant /bin/bash -c "set -- -f; source ~/dotfiles/bootstrap.sh"
 
 echo "WORKON_HOME=${VIRTUALENV_DIR}" >> ${USER_HOME}/.bash_profile
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> ${USER_HOME}/.bash_profile
-echo "cd ${PROJECT_SOURCE}" >> ${USER_HOME}/.bash_profile
 
 echo "Install the virtual environment.."
 mkdir ${VIRTUALENV_DIR}
 chown vagrant:vagrant ${VIRTUALENV_DIR}
 sudo su - vagrant /bin/bash -c "\
-    mkvirtualenv --python=`which python3` ${PROJECT_NAME};\
-    ln -s /vagrant ${PROJECT_SOURCE};\
-    pip install -r ${PROJECT_SOURCE}/requirements.txt"
+   mkvirtualenv --python=`which python3` ${PROJECT_NAME};\
+   ln -s /vagrant ${PROJECT_SOURCE};\
+   pip install -r ${PROJECT_SOURCE}/requirements.txt"
 
+echo "cd ${PROJECT_SOURCE}" >> ${USER_HOME}/.bash_profile
 echo "workon ${PROJECT_NAME}" >> ${USER_HOME}/.bash_profile
 
 echo "Configuring postgres.."
-sudo -u postgres psql -c "create user ${PROJECT_NAME} with password '{$PROJECT_NAME}';"
-sudo -u postgres psql -c "create database dev.${PROJECT_NAME};"
-sudo -u postgres psql -c "grant all privileges on database dev.${PROJECT_NAME} to ${PROJECT_NAME};"
+sudo -u postgres psql -c "create user ${PROJECT_NAME} with password '${PROJECT_NAME}';"
+sudo -u postgres psql -c "create database ${PROJECT_NAME};"
+sudo -u postgres psql -c "grant all privileges on database ${PROJECT_NAME} to ${PROJECT_NAME};"
 
 echo "Changing django project name.."
 sudo su - vagrant /bin/bash -c "\
-    git mv project_name/project_name project_name/${PROJECT_NAME}\
-    git mv project_name ${PROJECT_NAME}"
+    git mv ${PROJECT_SOURCE}/project_name/project_name ${PROJECT_SOURCE}/project_name/${PROJECT_NAME};\
+    git mv ${PROJECT_SOURCE}/project_name ${PROJECT_SOURCE}/${PROJECT_NAME};
+    git add ${PROJECT_SOURCE}/${PROJECT_NAME}"
 
 echo "Updating django configs.."
 sudo su - vagrant /bin/bash -c "\
-    sed -i 's/PROJECT_NAME/${PROJECT_NAME}/g' ${PROJECT_NAME}/settings/*.py;\
-    sed -i 's/PROJECT_HOST_NAME/${PROJECT_HOST_NAME}/g' ${PROJECT_NAME}/settings/*.py;\
-    sed -i 's/VIRTUAL_MACHINE_IP/${VM_IP}/g' ${PROJECT_NAME}/settings/*.py;"
+    sed -i 's/PROJECT_NAME/${PROJECT_NAME}/g' ${SETTINGS_FOLDER}/*.py;\
+    sed -i 's/PROJECT_HOST_NAME/${PROJECT_HOST_NAME}/g' ${SETTINGS_FOLDER}/*.py;\
+    sed -i 's/VIRTUAL_MACHINE_IP/${VM_IP}/g' ${SETTINGS_FOLDER}/*.py;"
 
 echo "Setting current django config.."
-sudo su - vagrant /bin/bash -c "ln -s ${PROJECT_NAME}/${PROJECT_NAME}/settings/dev.py ${PROJECT_NAME}/${PROJECT_NAME}/current_settings.py"
+sudo su - vagrant /bin/bash -c "ln -s ${SETTINGS_FOLDER}/dev.py ${PROJECT_NAME}/${PROJECT_NAME}/current_settings.py"
 
 echo "==============================="
 echo "Done!"
--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,7 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-PROJECT_NAME = "default"
+PROJECT_NAME = "project"
 PROJECT_HOST_NAME = PROJECT_NAME+".com"
 IP = "10.10.10.100"
 
@@ -9,7 +9,7 @@ Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.network "private_network", ip: IP
   config.vm.host_name = PROJECT_NAME+".dev"
-  config.vm.provision :shell, :path => ".install/vagrant_setup.sh", :args => PROJECT_NAME, PROJECT_HOST_NAME, IP
+  config.vm.provision :shell, :path => ".install/vagrant_setup.sh", :args => [PROJECT_NAME, PROJECT_HOST_NAME, IP]
 
   config.vm.define PROJECT_NAME
 
